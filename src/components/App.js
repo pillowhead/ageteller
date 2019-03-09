c91c50c3d578fe4b49383c6ce7673fa99b6988d4
pillowhead <gabriel.belmonte@gmail.com>
2019-03-09 11:02:54 +0100

    set up state

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,35 @@
 import React, { Component } from "react";
+import { Form, FormControl, Button } from "react-bootstrap";
+import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      newDate: "",
+      birthday: "1991-04-30"
+    };
+  }
+
+  changeBirthday() {
+    console.log(this.state);
+    this.setState({ birthday: this.state.newDate });
+  }
+
   render() {
-    return <div>ageteller component</div>;
+    return (
+      <div className="App">
+        <Form inline>
+          <h2>Input Your Birthday!</h2>
+          <FormControl
+            type="date"
+            onChange={event => this.setState({ newDate: event.target.value })}
+          />{" "}
+          <Button onClick={() => this.changeBirthday()}>Submit</Button>
+        </Form>
+      </div>
+    );
   }
 }
 
